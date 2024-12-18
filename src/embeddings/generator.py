d3cc088a767efea663893a228ae7244cd93e4c9f
JhonnyScrancho <solounaltra@gmail.com>
2024-12-18 14:16:08 +0100

    Update generator.py

--- src/embeddings/generator.py
+++ src/embeddings/generator.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from config import CHUNK_SIZE, CHUNK_OVERLAP
