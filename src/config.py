d42736a265f4ec717871990e1541757483c1f0b9
JhonnyScrancho <solounaltra@gmail.com>
2024-12-18 14:34:58 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ import streamlit as st
 # Constants
 EMBEDDING_MODEL = "text-embedding-ada-002"
 LLM_MODEL = "gpt-3.5-turbo"
-INDEX_NAME = "rag-p5eyqni"  # Nome dell'indice esistente
+INDEX_NAME = "rag"  # Nome dell'indice esistente
 CHUNK_SIZE = 1000
 CHUNK_OVERLAP = 200
 
