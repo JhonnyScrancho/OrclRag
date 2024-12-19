264c8a8b7a45b5ae16c4d21bd9eeb70b9535d898
JhonnyScrancho <solounaltra@gmail.com>
2024-12-19 13:39:08 +0100

    Update chain.py

--- src/rag/chain.py
+++ src/rag/chain.py
@@ -2,6 +2,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
+from src.config import LLM_MODEL
 
 def setup_rag_chain(retriever):
     """Configura la catena RAG."""
