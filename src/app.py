be0a22d392eea1802f8661b469ea0e994202bc93
JhonnyScrancho <solounaltra@gmail.com>
2024-12-19 12:20:11 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -7,7 +7,6 @@ from rag.retriever import PineconeRetriever
 from rag.chain import setup_rag_chain
 from ui.utils import display_thread_preview
 import pinecone
-from pinecone.core.client.configuration import Configuration as OpenApiConfiguration
 import hashlib
 from datetime import datetime
 from config import INDEX_NAME
